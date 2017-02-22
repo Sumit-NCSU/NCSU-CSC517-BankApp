8d1aad7373bc84e63f23a35e5cc28ba8955426fe
Sumit Srivastava <srivassumit@gmail.com>
2017-02-21 21:34:10 -0500

    trying friend table on postgresql

--- db/migrate/20170219081246_create_friends.rb
+++ db/migrate/20170219081246_create_friends.rb
@@ -1,8 +1,8 @@
 class CreateFriends < ActiveRecord::Migration[5.0]
   def change
     create_table :friends do |t|
-	  t.references :friend1, foreign_key: true
-      t.references :friend2, foreign_key: true
+	  t.references :friend, foreign_key: true
+      t.references :friend, foreign_key: true
       t.timestamps
     end
   end
