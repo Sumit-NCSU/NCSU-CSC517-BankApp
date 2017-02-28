d45bf10fafb83b5761f8aef706d5dc6cb404d1e2
Sumit Srivastava <srivassumit@gmail.com>
2017-02-27 20:29:50 -0500

    Changes for lend, borrow and withdraw -Katie

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(id: '0', name: 'Admin', email: 'admin@ncsu.edu', password: 'password', is_admin: 1)
 User.create(id: '1', name: 'User', email: 'user@ncsu.edu', password: 'password', is_admin: 0)
+User.create(id: '2', name: 'Another', email: 'user2@ncsu.edu', password: 'password', is_admin: 0)
+
+Friend.create(friend1_id: '2', friend2_id:'1')
+Friend.create(friend1_id: '1', friend2_id:'2')
+
+account1 = Account.create(id: '121212121', user_id: 1, status:'active', balance:1000)
+account2 = Account.create(id: '343434343', user_id: 2, status:'active', balance:2000)
+
+Transaction.create(id:1, txn_type:'send', amount:50, to_account_id:account1, from_account_id:account2, status:'pending')
